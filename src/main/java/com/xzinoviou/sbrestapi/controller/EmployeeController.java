bd71ef762872879c40c9dde884413884fa8be2f6
xzinoviou <xzinoviou@gmail.com>
2020-05-01 17:47:38 +0300

    initial commit

---
+++ src/main/java/com/xzinoviou/sbrestapi/controller/EmployeeController.java
@@ -0,0 +1,11 @@
+package com.xzinoviou.sbrestapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+
+}
---
+++ src/main/java/com/xzinoviou/sbrestapi/entity/Employee.java
@@ -0,0 +1,69 @@
+package com.xzinoviou.sbrestapi.entity;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "date_of_creation")
+    private Timestamp dateOfCreation;
+
+    public Employee() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Timestamp getDateOfCreation() {
+        return dateOfCreation;
+    }
+
+    public void setDateOfCreation(Timestamp dateOfCreation) {
+        this.dateOfCreation = dateOfCreation;
+    }
+}
