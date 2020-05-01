b8bbc5732d8a574bc1abb3116be02ba8bdf16e8e
xzinoviou <xzinoviou@gmail.com>
2020-05-01 19:37:38 +0300

    added employee controller

--- src/main/java/com/xzinoviou/sbrestapi/controller/EmployeeController.java
+++ src/main/java/com/xzinoviou/sbrestapi/controller/EmployeeController.java
@@ -1,11 +1,25 @@
 package com.xzinoviou.sbrestapi.controller;
 
+import com.xzinoviou.sbrestapi.entity.Employee;
+import com.xzinoviou.sbrestapi.service.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
 
+    private final EmployeeService employeeService;
 
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAll();
+    }
 }
